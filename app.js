86d5f88905a39d7d1f4e5e277d29bc165c7db874
blahblah <leesemin222@gmail.com>
2023-12-12 16:30:56 +0900

    session 완료

--- app.js
+++ app.js
@@ -29,4 +29,38 @@ const cors = require('cors')
 app.use(cors);
 
 //session & database
-const session =require('express-session')
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
+//아직 config 생성 X
+let options = {
+  host : process.env.NODE_ENV==='production' ? config.DATABASE_INFO.pro.HOST:config.DATABASE_INFO.dev.HOST,
+  port : process.env.NODE_ENV==='production' ? config.DATABASE_INFO.pro.PORT:config.DATABASE_INFO.dev.PORT,
+  user : process.env.NODE_ENV==='production' ? config.DATABASE_INFO.pro.USER:config.DATABASE_INFO.dev.USER,
+  password : process.env.NODE_ENV==='production' ? config.DATABASE_INFO.pro.PASSWORD:config.DATABASE_INFO.dev.PASSWORD,
+  database : process.env.NODE_ENV==='production' ? config.DATABASE_INFO.pro.DATABASE:config.DATABASE_INFO.dev.DATABASE,
+  expiration : 86400000, // 밀리초 => 24시간
+}
+
+let sessionStore = new MySQLStore(options)
+
+app.use(session({
+  key : 'S_DATA',
+  secret : `FFAFDSA#$$$ghewirEhdgoanfrhk@!`,
+  store:sessionStore,
+  resave : false,
+  saveUninitialized : false,
+}))
+
+app.use(async function(req, res, next){
+  if(typeof req.session.authority !== 'undefined'){
+    res.locals.isLogin = (req.session.authority > 0);
+    res.locals.userInfo = req.session.userInfo;
+    res.locals.auth = req.session.authority;
+  } else {
+    res.locals.isLogin = false;
+    res.locals.userInfo = {};
+  }
+  res.locals.url = req.url;
+  next();
+})
